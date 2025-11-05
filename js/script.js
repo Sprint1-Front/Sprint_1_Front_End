56f4cbd9092e02a0092f61f081c739fc35a42a24
Andre-Matuda <asmatuda@outlook.com>
2025-11-05 17:20:40 -0300

    script: criada constante que espande o main nav

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
     if (menuToggle && mainNav) {
         menuToggle.addEventListener('click', () => {
             mainNav.classList.toggle('active');
+
+            mainNav.classList.toggle('active');
+
+            const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
+            menuToggle.setAttribute('aria-expanded', String(!isExpanded));
+            
         });
     };
 });
