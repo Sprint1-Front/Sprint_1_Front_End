cb08900c6e9149aaf7473521dff769f927b7c802
guilheroliverfeitosa <guilhermeoliveirafeitosafacul@gmail.com>
2026-04-11 21:32:42 -0300

    feat: importando o Cadastro Aluno pro app

--- turma-do-bem/src/App.tsx
+++ turma-do-bem/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
+import CadastroAluno from './pages/CadastroAluno';
 import Doar from './pages/Doar';
 import Integrantes from './pages/Integrantes';
 import Cadastro from './pages/Cadastro';
